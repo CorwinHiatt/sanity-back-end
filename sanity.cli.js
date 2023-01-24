9e4a54413cc9efec51dc8fb231300a8030cb8efc
CorwinHiatt <corwinhiatt@yahoo.com>
2023-01-24 12:28:32 -0500

    feat: bootstrap sanity studio

---
+++ sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'qn91x42a',
+    dataset: 'production'
+  }
+})
---
+++ sanity.config.js
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'corwins-backend-portfolio',
+
+  projectId: 'qn91x42a',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
